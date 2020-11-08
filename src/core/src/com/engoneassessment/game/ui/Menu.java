4c8d65fd7dcdb0e2869e7aa1be43fc89ff47b084
tammyyeung <57869217+tammyyeung@users.noreply.github.com>
2020-11-08 21:07:55 +0800

    not done, UI

---
+++ src/core/src/com/engoneassessment/game/ui/Menu.java
@@ -0,0 +1,21 @@
+package com.engoneassessment.game.ui;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+
+public class Menu {
+
+    public double width;
+    public double height;
+
+    private Stage stage;
+    private Label label;
+
+}
