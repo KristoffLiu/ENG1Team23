6c6c1fe56f15d1e2e7b1dc41d7a3f06ca3936454
KristStudio <zhikangliu@hotmail.com>
2020-11-13 01:43:16 +0800

    fixing problems

--- src/desktop/src/com/engoneassessment/game/desktop/DesktopLauncher.java
+++ src/desktop/src/com/engoneassessment/game/desktop/DesktopLauncher.java
@@ -10,7 +10,6 @@ public class DesktopLauncher {
 
 		float scale = 0.6F;
 		config.title = "Auber Game - York CS ENG1 Assessment - Team 23";
-		config.
 		config.width = (int) (1920 * scale);
 		config.height = (int) (1080 * scale);
 
